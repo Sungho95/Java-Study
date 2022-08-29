d6c700cf1fb571061e510fe56c0ae54bfbcb5cc6
Sungho95 <tjdgh1785@gmail.com>
2022-08-29 18:15:33 +0900

    2022-08-29

---
+++ src/TypeEx.java
@@ -0,0 +1,12 @@
+public class TypeEx {
+    public static void main(String[] args) {
+        int primitive = 1;
+        Object reference = new Object();
+
+        System.out.println(primitive);
+        System.out.println(reference);
+
+        long bigNumber = 123456789123L;
+        System.out.println(bigNumber);
+    }
+}
--- src/test.java
+++ src/test.java
@@ -1,2 +1,6 @@
 public class test {
+    public static void main(String[] args) {
+        int ReSuLt_$1 = 5;
+        System.out.println(ReSuLt_$1);
+    }
 }
