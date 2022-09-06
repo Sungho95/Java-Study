dd5e86a48842ec466160eca9bc0805e543b16d6b
Sungho95 <tjdgh1785@gmail.com>
2022-09-06 20:40:50 +0900

    2022-09-06

--- src/test.java
+++ src/test.java
@@ -1,4 +1,23 @@
-public class test {
+public class test extends test2 {
     public static void main(String[] args) {
+        test t = new test();
+        test2 t2 = new test();
+
+        System.out.println(t.a);
+        System.out.println(t.b);
+        System.out.println(t.c);
+//        System.out.println(t.d);
+        System.out.println(t2.a);
+        System.out.println(t2.b);
+        System.out.println(t2.c);
+//        System.out.println(t2.d);
+
     }
+}
+
+class test2 {
+    public int a = 1;
+    protected int b = 2;
+    int c = 3;
+    private int d = 4;
 }
